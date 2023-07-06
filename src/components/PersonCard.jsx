afc76a217f84d5436fc24a37e3ba03ddb3361cd6
johnlee0115 <johnlee011500@gmail.com>
2023-07-05 21:53:26 -0700

    putting it together

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PersonCard = (props) => {
+    const [state, setState] = useState({
+        clickCount : 0
+    });
+
+    const handleClick = () => {
+        setState({
+            clickCount: state.clickCount + 1
+        });
+    }
+
     return (
         <div>
             <h1>{props.lastName}, {props.firstName}</h1>
-            <p>Age : {props.age}</p>
+            <p>Age : {props.age + state.clickCount}</p>
             <p>Hair Color : {props.hairColor}</p>
+            <button onClick={ handleClick }>Birthday Button for {props.firstName} {props.lastName}</button>
         </div>
     )
 }
